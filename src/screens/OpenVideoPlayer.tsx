3d77d1a7f4def10d23b307aad6479b166476e658
a-elnajjar <code@mail.com>
2023-07-25 21:31:29 -0400

    done work for video streaming

--- src/screens/OpenVideoPlayer.tsx
+++ src/screens/OpenVideoPlayer.tsx
@@ -5,13 +5,13 @@ import { View, Button,NativeModules } from 'react-native';
 const { VideoPlayerBridge } = NativeModules;
 
 export const renderVideoFromUrl = (urlString) => {
-  return VideoPlayerBridge.renderVideo(urlString);
+  return VideoPlayerBridge.renderVideoFromUrl(urlString);
 };
 
 export const OpenVideoPlayer = () => {
   const handlePlayVideo = async () => {
     try {
-      const videoUrl = 'YOUR_VIDEO_URL'; // Replace this with the actual video URL
+      const videoUrl = 'https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4'; // Replace this with the actual video URL
       const result = await renderVideoFromUrl(videoUrl);
       console.log('Video URL:', result);
     } catch (error) {
