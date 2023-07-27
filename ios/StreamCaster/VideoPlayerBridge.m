fddc44c9a23bfbfcd5858f28b0c943fc59c6a084
a-elnajjar <code@mail.com>
2023-07-26 23:36:48 -0400

    added references

--- ios/StreamCaster/VideoPlayerBridge.m
+++ ios/StreamCaster/VideoPlayerBridge.m
@@ -4,6 +4,7 @@
 //
 //  Created by Abdalla Elnajjar on 2023-07-26.
 //
+//https://gaitatzis.medium.com/react-native-native-modules-in-ios-swift-97eb9073f5a2
 
 #import <Foundation/Foundation.h>
 #import "React/RCTBridgeModule.h"
